4d2cb465ee2d7ad5b7c8a80b5c4cdab55d043b0a
HashEngineering <hashengineeringsolutions@gmail.com>
2021-07-22 13:52:10 -0700

    feat: support the executeProof merk function in Kotlin

--- src/main/kotlin/org/dashj/merk/MerkVerifyProof.kt
+++ src/main/kotlin/org/dashj/merk/MerkVerifyProof.kt
@@ -1,14 +1,16 @@
 package org.dashj.merk
 
-import org.dashevo.dpp.toBase58
-import org.dashevo.dpp.toHexString
-import org.dashevo.dpp.util.Cbor
-
+import org.dashj.platform.dpp.toBase58
+import org.dashj.platform.dpp.toHexString
+import org.dashj.platform.dpp.util.Cbor
 
 class MerkVerifyProof {
     companion object {
         @JvmStatic
-        private external fun verify(bytes: ByteArray, key: ByteArray, expectedHash: ByteArray): ByteArray
+        private external fun verify(bytes: ByteArray, expectedHash: ByteArray): ByteArray
+        @JvmStatic
+        private external fun extractProofNative(bytes: ByteArray, map: Any)
+        // private external fun extract_proof(bytes: ByteArray): Pair<ByteArray, Map<ByteArray, ByteArray>>
 
         @JvmStatic
         fun init() {
@@ -20,28 +22,41 @@ class MerkVerifyProof {
         }
 
         @JvmStatic
-        fun verifyProof(bytes: ByteArray, key: ByteArray, expectedHash: ByteArray): ByteArray {
-            return verify(bytes, key, expectedHash)
+        fun verifyProof(bytes: ByteArray, expectedHash: ByteArray): ByteArray {
+            return verify(bytes, expectedHash)
         }
 
+        @JvmStatic
+        fun extractProof(bytes: ByteArray): Map<ByteArrayKey, ByteArray> {
+            val map = HashMap<ByteArray, ByteArray>()
+            extractProofNative(bytes, map)
+            val mapReturn = HashMap<ByteArrayKey, ByteArray>()
+            for (entry in map) {
+                mapReturn[ByteArrayKey(entry.key)] = entry.value
+            }
+            return mapReturn
+        }
+
+        private val HASH_SIZE = 32
+
         @kotlin.ExperimentalUnsignedTypes
-        fun decode (bytes: ByteArray): Map<ByteArrayKey, ByteArray> {
+        fun decode(bytes: ByteArray): Map<ByteArrayKey, ByteArray> {
             var i = 0
             val result = hashMapOf<ByteArrayKey, ByteArray>()
             while (i < bytes.size) {
                 when (bytes[i].toInt()) {
                     1 -> {
                         i++
-                        val hash = bytes.copyOfRange(i, i + 20)
-                        i += 20
-                        println("Push(Hash(hash)) => 0x01 <20-byte hash>")
+                        val hash = bytes.copyOfRange(i, i + HASH_SIZE)
+                        i += HASH_SIZE
+                        println("Push(Hash(hash)) => 0x01 <32-byte hash>")
                         println("                 =>      ${hash.toHexString()}")
                     }
                     2 -> {
                         i++
-                        val hash = bytes.copyOfRange(i, i + 20)
-                        i += 20
-                        println("Push(KVHash(hash)) => 0x02 <20-byte hash>")
+                        val hash = bytes.copyOfRange(i, i + HASH_SIZE)
+                        i += HASH_SIZE
+                        println("Push(KVHash(hash)) => 0x02 <32-byte hash>")
                         println("                 =>        ${hash.toHexString()}")
                     }
                     3 -> {
@@ -50,9 +65,9 @@ class MerkVerifyProof {
                         i++
                         val key = bytes.copyOfRange(i, i + keySize)
                         i += keySize
-                        val valueSizeLow = bytes[i].toUByte().toInt()
-                        val valueSizeHigh = bytes[i+1].toUByte().toInt()
-                        val valueSize = valueSizeHigh * 256 + valueSizeLow;
+                        val valueSizeLow = bytes[i + 1].toUByte().toInt()
+                        val valueSizeHigh = bytes[i].toUByte().toInt()
+                        val valueSize = valueSizeHigh * 256 + valueSizeLow
                         i += 2
                         val value = bytes.copyOfRange(i, i + valueSize)
                         i += valueSize
@@ -81,12 +96,12 @@ class MerkVerifyProof {
             return result
         }
 
-        fun decodeRootHash (bytes: ByteArray): ByteArray? {
+        fun decodeRootHash(bytes: ByteArray): ByteArray? {
             var i = 0
             while (i < bytes.size) {
                 return when (bytes[i].toInt()) {
                     1 -> {
-                        i++;
+                        i++
                         val hash = bytes.copyOfRange(i, i + 20)
                         println(hash.toHexString())
                         hash
