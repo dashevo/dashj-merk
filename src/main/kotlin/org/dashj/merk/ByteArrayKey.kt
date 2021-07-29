3cbfefd78cb889fc99d71286c095296771065e61
HashEngineering <hashengineeringsolutions@gmail.com>
2021-07-29 09:09:54 -0700

    chore: make toString output hex; reformat

--- src/main/kotlin/org/dashj/merk/ByteArrayKey.kt
+++ src/main/kotlin/org/dashj/merk/ByteArrayKey.kt
@@ -1,11 +1,13 @@
 package org.dashj.merk
 
+import org.dashj.platform.dpp.toHexString
+
 class ByteArrayKey(private val bytes: ByteArray) {
     override fun equals(other: Any?): Boolean =
         this === other || other is ByteArrayKey && this.bytes contentEquals other.bytes
 
     override fun hashCode(): Int = bytes.contentHashCode()
-    override fun toString(): String = bytes.contentToString()
+    override fun toString(): String = bytes.toHexString()
     fun toByteArray(): ByteArray {
         return bytes; }
 }
